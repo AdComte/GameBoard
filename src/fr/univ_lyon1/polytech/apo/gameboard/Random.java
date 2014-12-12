7a3ac7d28d576e010f18d15e64e32987b1dea398
p1206512 <p1206512@EPULWKS44.univ-lyon1.fr>
2014-12-12 16:10:10 +0100

    Ajout de la classe random

---
+++ src/fr/univ_lyon1/polytech/apo/gameboard/Random.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package fr.univ_lyon1.polytech.apo.gameboard;
+
+/**
+ *
+ * @author p1206512
+ */
+public class Random extends Player {
+    
+}
